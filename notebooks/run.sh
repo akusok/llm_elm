a0571f9d40174ebb16c2e57e6cf90e7d557d850b
Anton Akusok <akusok@users.noreply.github.com>
2025-04-19 22:33:51 +0300

    Add data

--- notebooks/run.sh
+++ notebooks/run.sh
@@ -1,6 +1,36 @@
 # Run the command 10 times in parallel
 for i in {1..5}; do
-  uv run python text_evals.py cogito:8b 10 &
+  uv run python text_evals.py qwen2.5-coder:1.5b 10 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..5}; do
+  uv run python text_evals.py cogito:3b 15 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..5}; do
+  uv run python text_evals.py granite3.3:2b 15 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..5}; do
+  uv run python text_evals.py qwen2.5-coder:3b 18 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..5}; do
+  uv run python text_evals.py granite3.3 18 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..5}; do
+  uv run python text_evals.py llama3.1 18 &
 done
 wait
 
@@ -11,25 +41,50 @@ done
 wait
 
 # Run the command 10 times in parallel
-for i in {1..4}; do
-  uv run python text_evals.py cogito:14b 5 &
+for i in {1..5}; do
+  uv run python text_evals.py cogito:8b 10 &
 done
 wait
 
 # Run the command 10 times in parallel
-for i in {1..4}; do
-  uv run python text_evals.py qwen2.5-coder:14b 5 &
+for i in {1..5}; do
+  uv run python text_evals.py phi4 16 &
+done
+wait
+
+
+# Run the command 10 times in parallel
+for i in {1..2}; do
+  uv run python text_evals.py cogito:14b 40 &
 done
 wait
 
 # Run the command 10 times in parallel
 for i in {1..2}; do
-  uv run python text_evals.py cogito:32b 5 &
+  uv run python text_evals.py qwen2.5-coder:14b 40 &
 done
 wait
 
 # Run the command 10 times in parallel
 for i in {1..2}; do
-  uv run python text_evals.py qwen2.5-coder:32b 5 &
+  uv run python text_evals.py cogito:32b 15 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..2}; do
+  uv run python text_evals.py qwen2.5-coder:32b 15 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..2}; do
+  uv run python text_evals.py cogito:32b 30 &
+done
+wait
+
+# Run the command 10 times in parallel
+for i in {1..2}; do
+  uv run python text_evals.py qwen2.5-coder:32b 30 &
 done
 wait
